e082bba1bfe27635a1196747d90b887fa71c671b
Avery Harnish <averyharnish@gmail.com>
2022-01-28 15:20:57 -0600

    feat: updates versions in build.rs

--- xtask/src/tools/npm.rs
+++ xtask/src/tools/npm.rs
@@ -6,55 +6,29 @@ use std::{collections::HashMap, str};
 
 use crate::{
     tools::Runner,
-    utils::{CommandOutput, PKG_PROJECT_ROOT},
+    utils::{self, CommandOutput},
 };
 
 pub(crate) struct NpmRunner {
     runner: Runner,
-    package_directories: HashMap<String, Utf8PathBuf>,
+    harmonizer_directories: HashMap<String, Utf8PathBuf>,
 }
 
 impl NpmRunner {
     pub(crate) fn new(verbose: bool) -> Result<Self> {
+        Self::require_volta()?;
         let runner = Runner::new("npm", verbose)?;
-        let project_root = PKG_PROJECT_ROOT.clone();
 
-        let mut package_directories = HashMap::with_capacity(2);
+        let harmonizer_directories = utils::get_harmonizer_crates()
+            .context("Could not find one or more required packages")?;
 
-        package_directories.insert(
-            "harmonizer-0".to_string(),
-            project_root.join("harmonizer-0"),
-        );
-        package_directories.insert(
-            "harmonizer-2".to_string(),
-            project_root.join("harmonizer-2"),
-        );
-
-        let mut pkg_errs = Vec::new();
-        for (package_name, package_directory) in &package_directories {
-            if !package_directory.exists() {
-                pkg_errs.push(format!(
-                    "{} does not exist at {}.",
-                    package_name, package_directory
-                ));
-            }
-        }
-        if let Some(first_pkg_err) = pkg_errs.pop() {
-            let mut final_err = anyhow!(first_pkg_err);
-            for pkg_err in pkg_errs {
-                final_err = final_err.context(pkg_err);
-            }
-            Err(final_err.context("Could not find one or more required npm packages."))
-        } else {
-            Ok(Self {
-                runner,
-                package_directories,
-            })
-        }
+        Ok(Self {
+            runner,
+            harmonizer_directories,
+        })
     }
 
     pub(crate) fn lint(&self) -> Result<()> {
-        self.require_volta()?;
         self.run_all(&["install"])
             .context("Could not install all dependencies")?;
         self.run_all(&["run", "lint"])
@@ -63,14 +37,14 @@ impl NpmRunner {
         Ok(())
     }
 
-    fn require_volta(&self) -> Result<()> {
+    fn require_volta() -> Result<()> {
         which("volta")
             .map(|_| ())
-            .map_err(|_| anyhow!("You must have `volta` installed to run this command."))
+            .map_err(|_| anyhow!("You must have `volta` installed."))
     }
 
     fn run_all(&self, args: &[&str]) -> Result<()> {
-        for (pkg_name, pkg_directory) in &self.package_directories {
+        for (pkg_name, pkg_directory) in &self.harmonizer_directories {
             self.npm_exec(args, pkg_directory)
                 .with_context(|| format!("Could not run command for `{}`", pkg_name))?;
         }
