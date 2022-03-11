ae16a4ebb7cc56a870264d48354edad148892b40
Jeremy Lempereur <jeremy.lempereur@gmail.com>
2022-03-11 16:21:59 +0100

    fix: generate deno snapshots in build.rs (#65)
    In #58 I added a snapshots generation phase to our harmonizer packages.
    
    However I created a bug because the snapshot generation phase would happen when the harmonizer-0 and harmonizer-2 packages are generated.
    
    This means downstream users who would rely on published harmonizer crates would get snapshots generated by an other computer / architecture.
    
    This triggered a bug that would fail any harmonizer call when relying on the published crate.
    
    This commit adds a build.rs script to the published crates, so that snapshots are created when downstream binaries are built.

--- build_harmonizer.rs
+++ build_harmonizer.rs
@@ -1,10 +1,7 @@
 use deno_core::{JsRuntime, RuntimeOptions};
 use semver::Version;
 use serde_json::Value as JsonValue;
-use std::error::Error;
-use std::fs::{read_to_string, File};
-use std::io::Write;
-use std::{env, fs, path::Path, process::Command, str};
+use std::{env, error::Error, fs, io::Write, path::Path, process::Command, str};
 use toml_edit::{value as new_toml_value, Document as TomlDocument};
 
 // this build.rs file is used by both `harmonizer-0` and `harmonizer-2`
@@ -22,6 +19,9 @@ fn main() {
 
     update_manifests();
     bundle_for_deno();
+    // we still need to create deno snapshots in order to run tests locally
+    // the actual deno snapshot creation for downstream users will happen in the respective
+    // harmonizer-0 and harmonizer-2 build.rs file
     create_snapshot().expect("unable to create v8 snapshot: query_runtime.snap");
 }
 
@@ -254,20 +254,20 @@ fn create_snapshot() -> Result<(), Box<dyn Error>> {
 
     // The runtime automatically contains a Deno.core object with several
     // functions for interacting with it.
-    let runtime_source = read_to_string("deno/runtime.js")?;
+    let runtime_source = fs::read_to_string("deno/runtime.js")?;
     runtime
         .execute_script("<init>", &runtime_source)
         .expect("unable to initialize harmonizer runtime environment");
 
     // Load the composition library.
-    let composition_source = read_to_string("dist/composition.js")?;
+    let composition_source = fs::read_to_string("dist/composition.js")?;
     runtime
         .execute_script("composition.js", &composition_source)
         .expect("unable to evaluate composition module");
 
     // Create our base query snapshot which will be included in
     // src/js.rs to initialise our JsRuntime().
-    let mut snap = File::create("snapshots/query_runtime.snap")?;
+    let mut snap = fs::File::create("snapshots/query_runtime.snap")?;
     snap.write_all(&runtime.snapshot())?;
 
     Ok(())
