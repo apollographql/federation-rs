a71cce36b793d653e96d0e7d5236b2abbb896a63
Avery Harnish <averyharnish@gmail.com>
2022-04-05 13:35:12 -0500

    feat: allows pinning versions in SupergraphConfig (#93)
    * feat: allows pinning versions in SupergraphConfig
    
    * chore: fixup tests

--- apollo-federation-types/src/config/version.rs
+++ apollo-federation-types/src/config/version.rs
@@ -1,5 +1,6 @@
 use crate::config::ConfigError;
 
+use semver::Version;
 use serde::{Deserialize, Serialize};
 
 use std::{
@@ -7,23 +8,58 @@ use std::{
     str::FromStr,
 };
 
-#[derive(Debug, Copy, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub enum FederationVersion {
-    FedOne,
-    FedTwo,
+    LatestFedOne,
+    LatestFedTwo,
+    ExactFedOne(Version),
+    ExactFedTwo(Version),
+}
+
+impl FederationVersion {
+    pub fn get_major_version(&self) -> u64 {
+        match self {
+            Self::LatestFedOne | Self::ExactFedOne(_) => 0,
+            Self::LatestFedTwo | Self::ExactFedTwo(_) => 2,
+        }
+    }
+
+    pub fn get_exact(&self) -> Option<&Version> {
+        match self {
+            Self::ExactFedOne(version) | Self::ExactFedTwo(version) => Some(version),
+            _ => None,
+        }
+    }
+
+    pub fn is_fed_one(&self) -> bool {
+        matches!(self, Self::LatestFedOne) || matches!(self, Self::ExactFedOne(_))
+    }
+
+    pub fn is_fed_two(&self) -> bool {
+        matches!(self, Self::LatestFedTwo) || matches!(self, Self::ExactFedTwo(_))
+    }
+
+    pub fn get_tarball_version(&self) -> String {
+        match self {
+            Self::LatestFedOne => "latest-0".to_string(),
+            Self::LatestFedTwo => "latest-2".to_string(),
+            Self::ExactFedOne(v) | Self::ExactFedTwo(v) => format!("v{}", v),
+        }
+    }
 }
 
 impl Default for FederationVersion {
     fn default() -> Self {
-        FederationVersion::FedTwo
+        FederationVersion::LatestFedOne
     }
 }
 
 impl Display for FederationVersion {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let result = match self {
-            Self::FedOne => "0",
-            Self::FedTwo => "2",
+            Self::LatestFedOne => "0".to_string(),
+            Self::LatestFedTwo => "2".to_string(),
+            Self::ExactFedOne(version) | Self::ExactFedTwo(version) => format!("={}", version),
         };
         write!(f, "{}", result)
     }
@@ -33,12 +69,35 @@ impl FromStr for FederationVersion {
     type Err = ConfigError;
 
     fn from_str(input: &str) -> std::result::Result<Self, Self::Err> {
-        match input {
-            "0" | "1" => Ok(Self::FedOne),
-            "2" => Ok(Self::FedTwo),
-            _ => Err(ConfigError::InvalidConfiguration {
-                message: format!("Specified version `{}` is not supported", input),
-            }),
+        let invalid_version = ConfigError::InvalidConfiguration {
+            message: format!("Specified version `{}` is not supported. You can either specify '1', '2', or a fully qualified version prefixed with an '=', like: =2.0.0", input),
+        };
+        if input.len() > 1 && (input.starts_with('=') || input.starts_with('v')) {
+            if let Ok(version) = input[1..].parse::<Version>() {
+                if version.major == 0 {
+                    if version.minor >= 36 {
+                        Ok(Self::ExactFedOne(version))
+                    } else {
+                        Err(ConfigError::InvalidConfiguration { message: format!("Specified version `{}` is not supported. The earliest version you can specify for federation 1 is '=0.36.0'", input) })
+                    }
+                } else if version.major == 2 {
+                    if version >= "2.0.0-preview.9".parse::<Version>().unwrap() {
+                        Ok(Self::ExactFedTwo(version))
+                    } else {
+                        Err(ConfigError::InvalidConfiguration { message: format!("Specified version `{}` is not supported. The earliest version you can specify for federation 2 is '=2.0.0-preview.9'", input) })
+                    }
+                } else {
+                    Err(invalid_version)
+                }
+            } else {
+                Err(invalid_version)
+            }
+        } else {
+            match input {
+                "0" | "1" | "latest-0" | "latest-1" => Ok(Self::LatestFedOne),
+                "2" | "latest-2" => Ok(Self::LatestFedTwo),
+                _ => Err(invalid_version),
+            }
         }
     }
 }
