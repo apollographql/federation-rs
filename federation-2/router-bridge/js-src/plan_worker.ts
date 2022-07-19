822c6ec31303fe7cc9b7d6c5de697f724a984b8b
Geoffroy Couprie <geoffroy@apollographql.com>
2022-07-19 15:24:40 +0200

    add missing usage reporting field in errors

--- federation-2/router-bridge/js-src/plan_worker.ts
+++ federation-2/router-bridge/js-src/plan_worker.ts
@@ -184,11 +184,23 @@ async function run() {
         }
       } catch (e) {
         logger.warn(`an error happened in the worker runtime ${e}\n`);
-        await send({ id, payload: { errors: [e] } });
+        await send({ id, payload: {
+          errors: [e],
+          usageReporting: {
+            statsReportKey: "",
+            referencedFieldsByType: {},
+          },
+        } });
       }
     } catch (e) {
       logger.warn(`plan_worker: an unknown error occured ${e}\n`);
-      await send({ payload: { errors: [e] } });
+      await send({ payload: {
+        errors: [e],
+        usageReporting: {
+          statsReportKey: "",
+          referencedFieldsByType: {},
+        },
+      } });
     }
   }
 }
