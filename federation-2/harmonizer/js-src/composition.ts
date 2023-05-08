ff3ef6137500be47b044ed4b284e461ef85c7c10
Michael Watson <xambrew@gmail.com>
2023-05-08 16:13:43 -0500

    Add error locations for `rover supergraph compose` (#218)
    Co-authored-by: Avery Harnish <avery@apollographql.com>
    Co-authored-by: Jesse Rosenberger <git@jro.cc>

--- federation-2/harmonizer/js-src/composition.ts
+++ federation-2/harmonizer/js-src/composition.ts
@@ -1,5 +1,11 @@
-import { composeServices, CompositionResult } from "@apollo/composition";
-import { parse } from "graphql";
+import { composeServices } from "@apollo/composition";
+import { parse, Token } from "graphql";
+import {
+  BuildErrorNode,
+  CompositionError,
+  CompositionResult,
+  Position,
+} from "./types";
 
 export function composition(
   serviceList: { sdl: string; name: string; url?: string }[]
@@ -8,12 +14,68 @@ export function composition(
     throw new Error("Error in JS-Rust-land: serviceList missing or incorrect.");
   }
 
+  serviceList.some((service) => {
+    if (
+      typeof service.name !== "string" ||
+      !service.name ||
+      (typeof service.url !== "string" && service.url) ||
+      (typeof service.sdl !== "string" && service.sdl)
+    ) {
+      throw new Error("Missing required data structure on service.");
+    }
+  });
+
   let subgraphList = serviceList.map(({ sdl, ...rest }) => ({
     typeDefs: parseTypedefs(sdl),
     ...rest,
   }));
 
-  return composeServices(subgraphList);
+  const composed = composeServices(subgraphList);
+  let hints: { message: string }[] = [];
+  if (composed.hints) {
+    composed.hints.map((composed_hint) => {
+      hints.push({ message: composed_hint.toString() });
+    });
+  }
+
+  if (composed.errors) {
+    //We need to reshape the errors
+    let errors: CompositionError[] = [];
+    composed.errors.map((err) => {
+      let nodes: BuildErrorNode[] = [];
+      err.nodes.map((node) => {
+        nodes.push({
+          subgraph: (node as any)?.subgraph,
+          source: node?.loc.source.body,
+          start: getPosition(node.loc.startToken),
+          end: getPosition(node.loc.endToken),
+        });
+      });
+
+      errors.push({
+        code: (err?.extensions["code"] as string) ?? "",
+        message: err.message,
+        nodes,
+      });
+    });
+
+    return { Err: errors };
+  } else
+    return {
+      Ok: {
+        supergraphSdl: composed.supergraphSdl,
+        hints,
+      },
+    };
+}
+
+function getPosition(token: Token): Position {
+  return {
+    start: token.start,
+    end: token.end,
+    line: token.line,
+    column: token.column,
+  };
 }
 
 //@ts-ignore
