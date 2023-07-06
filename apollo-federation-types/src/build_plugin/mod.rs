7f6be3993731673bcc707922e3e6f03f82860f53
Joshua Segaran <josh.segaran@apollographql.com>
2023-07-06 08:19:30 -0700

    Pub more types

--- apollo-federation-types/src/build_plugin/mod.rs
+++ apollo-federation-types/src/build_plugin/mod.rs
@@ -3,4 +3,6 @@ mod plugin_result;
 
 pub use build_message::BuildMessage;
 pub use build_message::BuildMessageLevel;
+pub use build_message::BuildMessageLocation;
+pub use build_message::BuildMessagePoint;
 pub use plugin_result::PluginResult;
