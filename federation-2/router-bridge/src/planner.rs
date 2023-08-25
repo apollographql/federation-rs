853c7f1376bdca125d9ca752f6678a7af8e8d001
Trevor Scheer <trevor.scheer@gmail.com>
2023-08-25 13:10:07 -0700

    fmt

--- federation-2/router-bridge/src/planner.rs
+++ federation-2/router-bridge/src/planner.rs
@@ -182,7 +182,7 @@ impl PlannerError {
     /// Return true if the error was a GraphQL validation error.
     pub fn is_validation_error(&self) -> bool {
         let PlannerError::WorkerGraphQLError(err) = self else {
-            return false
+            return false;
         };
         err.validation_error
     }
