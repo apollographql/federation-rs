6ef4f11fdaffaa415ec4d05941afe6d2fae8a258
Sachin D. Shinde <sachin@apollographql.com>
2023-08-30 10:40:13 -0700

    Update code to add all versions of authenticated/requiresScopes

--- federation-2/router-bridge/js-src/supported_features.ts
+++ federation-2/router-bridge/js-src/supported_features.ts
@@ -1,28 +1,28 @@
 import {
   AUTHENTICATED_VERSIONS,
   DEFAULT_SUPPORTED_SUPERGRAPH_FEATURES,
-  FeatureVersion,
+  FeatureDefinition,
+  FeatureDefinitions,
   REQUIRES_SCOPES_VERSIONS,
 } from "@apollo/federation-internals";
 
-const authenticatedV01Feature = AUTHENTICATED_VERSIONS.find(
-  new FeatureVersion(0, 1)
-);
-if (!authenticatedV01Feature) {
-  throw Error(
-    "Federation package unexpectedly did not contain authenticated v0.1 spec."
-  );
-}
-const requiresScopesV01Feature = REQUIRES_SCOPES_VERSIONS.find(
-  new FeatureVersion(0, 1)
-);
-if (!requiresScopesV01Feature) {
-  throw new Error(
-    "Federation package unexpectedly did not contain requiresScopes v0.1 spec."
-  );
-}
 export const ROUTER_SUPPORTED_SUPERGRAPH_FEATURES: Set<string> = new Set(
   DEFAULT_SUPPORTED_SUPERGRAPH_FEATURES
-)
-  .add(authenticatedV01Feature.toString())
-  .add(requiresScopesV01Feature.toString());
+);
+
+function addToRouterFeatures<T extends FeatureDefinition>(
+  definitions: FeatureDefinitions<T>
+) {
+  definitions.versions().forEach((version) => {
+    const feature = definitions.find(version);
+    if (!feature) {
+      throw Error(
+        `Federation package unexpectedly did not contain feature spec ${definitions.identity}/${version}`
+      );
+    }
+    ROUTER_SUPPORTED_SUPERGRAPH_FEATURES.add(feature.toString());
+  });
+}
+
+addToRouterFeatures(AUTHENTICATED_VERSIONS);
+addToRouterFeatures(REQUIRES_SCOPES_VERSIONS);
