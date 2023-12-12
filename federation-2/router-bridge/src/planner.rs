a3e5d8ce207682a00efd807e34a1904d2638168a
Geoffroy Couprie <apollo@geoffroycouprie.com>
2023-12-12 10:00:38 +0100

    Update federation-2/router-bridge/src/planner.rs
    Co-authored-by: Renée <renee@kooi.me>

--- federation-2/router-bridge/src/planner.rs
+++ federation-2/router-bridge/src/planner.rs
@@ -725,7 +725,7 @@ pub struct QueryPlannerDebugConfig {
     /// debug a specific issue (with unexpectedly slow query planning for instance). Remember that
     /// setting this value too low can negatively affect query runtime (due to the use of sub-optimal
     /// query plans).
-    max_evaluatedplans: Option<u32>,
+    max_evaluated_plans: Option<u32>,
 
     /// Before creating query plans, for each path of fields in the query we compute all the
     /// possible options to traverse that path via the subgraphs. Multiple options can arise because
