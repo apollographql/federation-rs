feca4451d679e295650e871efac1f935d7dcd725
Jeremy Lempereur <jeremy.lempereur@iomentum.com>
2024-07-29 10:22:23 +0200

    Send payload id if applicable when an exception is caught (#545)
    If an exception is caught during planning, we previously didn't send the
    messageId.
    
    The response could thus not be properly routed back to the proper
    sender, and would lead to such errors as:
    
    ```
    value: TypeError: Error parsing args at position 1: missing field `id`
    ```
    
    This changeset adds the messageId if possible.
    
    ---------
    
    Co-authored-by: Renée <renee@kooi.me>

--- router-bridge/js-src/plan_worker.ts
+++ router-bridge/js-src/plan_worker.ts
@@ -245,8 +245,13 @@ const updateQueryPlanner = (
 
 async function run() {
   while (true) {
+    // If an Exception is raised,
+    // there won't be any messageId to send a payload back to the router,
+    // unless we keep it in this scope.
+    let messageId = "";
     try {
       const { id, payload: event } = await receive();
+      messageId = id;
       try {
         switch (event?.kind) {
           case PlannerEventKind.UpdateSchema:
@@ -336,6 +341,7 @@ async function run() {
       };
 
       await send({
+        id: messageId,
         payload: {
           errors: [unexpectedError],
           usageReporting: {
